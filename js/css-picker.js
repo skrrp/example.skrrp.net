b5d33b9505f88189495baa0556a69efdb0bff7b3
Joe Hockaday <jdhockad@hotmail.com>
2024-03-15 16:30:15 +0100

    Tidied box style

--- js/css-picker.js
+++ js/css-picker.js
@@ -6,10 +6,10 @@ function setUpCss() {
 
 function getCss(type) {
   const css = {
-    'light-hi': function() { return 'body:{background-color:#ffffff;color:#000000;}input{background-color:#ffffff;color:#000000;}a:link{color:#0066cc;}a:visited{color:#5518ab;}a:hover{color:#0066cc;}a:active{color:#ee0000;}a.no-href{all:unset;cursor:pointer;color:#000000;}.grey-box{background-color:#dcdcdc;padding:5px;}'; },
-    'light-lo': function() { return 'body{background-color:#eeeeee;color:#333333;}input{background-color:#eeeeee;color:#333333;}a:link{color:#0055bb;}a:visited{color:#44089a;}a:hover{color:#0055bb;}a:active{color: #dd0000;}a.no-href{all:unset;cursor:pointer;color:#333333;}.grey-box{background-color:#bababa;padding:5px;}'; },
-    'dark-lo': function() { return 'body{background-color:#333333;color:#dddddd;}input{background-color:#333333;color:#dddddd;}a:link{color:#ffaa44;}a:visited{color:#bbf675;}a:hover{color:#ffaa44;}a:active{color:#220000;}a.no-href{all:unset;cursor:pointer;color:#dddddd;}.grey-box{background-color:#242424;padding:5px;}'; },
-    'dark-hi': function() { return 'body{background-color:#000000;color:#ffffff;}input{background-color:#000000;color:#ffffff;}a:link{color:#ff7744;}a:visited{color:#ccf275;}a:hover{color:#ff7744;}a:active{color:#22ffff;}a.no-href{all:unset;cursor:pointer;color:#ffffff;}.grey-box{background-color:#121212;padding:5px;}'; },
+    'light-hi': function() { return 'body:{background-color:#ffffff;color:#000000;}input{background-color:#ffffff;color:#000000;}a:link{color:#0066cc;}a:visited{color:#5518ab;}a:hover{color:#0066cc;}a:active{color:#ee0000;}a.no-href{all:unset;cursor:pointer;color:#000000;}.grey-box{background-color:#dcdcdc;padding:1px;}'; },
+    'light-lo': function() { return 'body{background-color:#eeeeee;color:#333333;}input{background-color:#eeeeee;color:#333333;}a:link{color:#0055bb;}a:visited{color:#44089a;}a:hover{color:#0055bb;}a:active{color: #dd0000;}a.no-href{all:unset;cursor:pointer;color:#333333;}.grey-box{background-color:#bababa;padding:1px;}'; },
+    'dark-lo': function() { return 'body{background-color:#333333;color:#dddddd;}input{background-color:#333333;color:#dddddd;}a:link{color:#ffaa44;}a:visited{color:#bbf675;}a:hover{color:#ffaa44;}a:active{color:#220000;}a.no-href{all:unset;cursor:pointer;color:#dddddd;}.grey-box{background-color:#242424;padding:1px;}'; },
+    'dark-hi': function() { return 'body{background-color:#000000;color:#ffffff;}input{background-color:#000000;color:#ffffff;}a:link{color:#ff7744;}a:visited{color:#ccf275;}a:hover{color:#ff7744;}a:active{color:#22ffff;}a.no-href{all:unset;cursor:pointer;color:#ffffff;}.grey-box{background-color:#121212;padding:1px;}'; },
   };
   if (!type in css) type = 'light-hi';
   return css[type]();
